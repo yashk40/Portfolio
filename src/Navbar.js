99178256100be550483f5d96086920b053e3e665
yash <159629707+yashk40@users.noreply.github.com>
2025-04-25 01:40:10 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,12 +11,12 @@ export default function Navbar() {
                 <a href="https://github.com/yashk40"><img id="image-nav" src="Github.svg" alt="Github"/></a>
                 <a href="https://www.linkedin.com/in/yash-kum-658a252aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img id="image-nav" src="linkdin.svg" alt="LinkedIn"/></a>
                 <a href="ykumawat006@gmail.com"><img id="image-nav" src="Mail.svg" alt="Email"/></a>     
-                <a href="https://drive.google.com/file/d/1F0jCYb8w-pAVMQwubVdniRPb5K2Q3dPt/view?usp=sharing"><img id="image-nav" src="Page.svg" alt="Resume"/></a>    
+                <a href="https://drive.google.com/file/d/1cuA1jMz3FttHmh0zL9x9zYCptYx-PUIv/view?usp=sharing"><img id="image-nav" src="Page.svg" alt="Resume"/></a>    
             </div>
             <div id='Navbar-Links-2'>
                 <a href='https://github.com/yashk40' id='github'>Github</a>
                 <a href='https://www.linkedin.com/in/yash-kum-658a252aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' id='linkedin'>Linkedin</a>
-                <a href='https://drive.google.com/file/d/1F0jCYb8w-pAVMQwubVdniRPb5K2Q3dPt/view?usp=sharing' id='Resume'>Resume</a>
+                <a href='https://drive.google.com/file/d/1cuA1jMz3FttHmh0zL9x9zYCptYx-PUIv/view?usp=sharing' id='Resume'>Resume</a>
             </div>
             <p id="Gmail">Ykumawat006@gmail.com</p>
           

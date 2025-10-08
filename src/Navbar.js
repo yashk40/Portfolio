121d381fafb0a97f4e192c5102a4a363606d4def
yash <159629707+yashk40@users.noreply.github.com>
2025-10-08 11:21:49 +0530

    auto-55509

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,13 +4,13 @@ import "./App.css"
 export default function Navbar() {
   return (
     <div>
-          <div id="Navbar">
+          <div id="Navbar" >
         <div id="Heading-Space">
             <p id="Heading">Yash.</p>
             <div id="image-Space">
                 <a href="https://github.com/yashk40"><img id="image-nav" src="Github.svg" alt="Github"/></a>
                 <a href="https://www.linkedin.com/in/yash-kum-658a252aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img id="image-nav" src="linkdin.svg" alt="LinkedIn"/></a>
-                <a href="ykumawat006@gmail.com"><img id="image-nav" src="Mail.svg" alt="Email"/></a>     
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=Ykumawat006@gmail.com" target="_blank" rel="noopener noreferrer"><img id="image-nav" src="Mail.svg" alt="Email"/></a>     
                 <a href="https://drive.google.com/file/d/1r8ZWLx9k1c7qAcbBAWDs_NRueqzaXhq-/view?usp=sharing"><img id="image-nav" src="Page.svg" alt="Resume"/></a>    
             </div>
             <div id='Navbar-Links-2'>
@@ -18,7 +18,7 @@ export default function Navbar() {
                 <a href='https://www.linkedin.com/in/yash-kum-658a252aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' id='linkedin'>Linkedin</a>
                 <a href='https://drive.google.com/file/d/1r8ZWLx9k1c7qAcbBAWDs_NRueqzaXhq-/view?usp=sharing' id='Resume'>Resume</a>
             </div>
-            <p id="Gmail">Ykumawat006@gmail.com</p>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=Ykumawat006@gmail.com" target="_blank" rel="noopener noreferrer" id="Gmail" style={{color:"#F9F0E1"}}>Ykumawat006@gmail.com</a>
           
         </div>
         
